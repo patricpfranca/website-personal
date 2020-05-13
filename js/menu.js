f9d6eb77ca6e041db951f8c5402fb7229e8b3449
Patric Pereira <patric.pereirafranca@gmail.com>
2020-05-13 13:57:15 -0300

    refactoring js mobile menu

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,18 @@ let show = true;
 
 const menuSection = document.querySelector(".menu-section");
 const menuToggle = menuSection.querySelector(".menu-toggle");
+const itemMenu = document.querySelector(".item-menu");
 
 menuToggle.addEventListener("click", () => {
   document.body.style.overflow = show ? "hidden" : "initial";
 
   menuSection.classList.toggle("on", show);
   show = !show;
+});
+
+document.getElementById("list").addEventListener("click", (e) => {
+  if (e.target && e.target.matches("li a.item-menu")) {
+    menuSection.classList.toggle("on", show);
+    show = !show;
+  }
 });
