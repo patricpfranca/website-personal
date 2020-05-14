604ddeb843c24749eecd9648ae09c109ce879ce9
Patric Pereira <patric.pereirafranca@gmail.com>
2020-05-13 23:32:23 -0300

    include typewritter

--- js/index.js
+++ js/index.js
@@ -8,17 +8,52 @@ window.addEventListener("scroll", () => {
   }
 });
 
-window.onload = () => {
-  const texts = ["Patric Pereira", "Developer"];
-  let actual = 0;
-
-  const elChangeText = document.getElementById("change-text");
-
-  elChangeText.innerHTML = texts[actual++];
-
-  setInterval(() => {
-    if (actual >= texts.length) actual = 0;
-
-    elChangeText.innerHTML = texts[actual++];
-  }, 4000);
+const TypeWriter = function (txtElement, words, waitTime = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(waitTime, 10);
+  this.type();
+  this.isDeleting = false;
 };
+
+TypeWriter.prototype.type = function () {
+  const current = this.wordIndex % this.words.length;
+  const fullTxt = this.words[current];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  let typeSpeed = 300;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.wordIndex++;
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+
+  new TypeWriter(txtElement, words, wait);
+}
